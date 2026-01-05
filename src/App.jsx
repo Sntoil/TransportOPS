14eb2c6e31ff5678ddb1e9be107605d83cedfdbb
Rammragrean <fancescotottaey@gmail.com>
2026-01-05 23:47:37 +0700

    Update V3.3

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ export default function TransportApp() {
     handleSaveRule, handleDeleteRule,
     handleSaveManualScore, handleDeleteManualScore,
     handleSaveRole
-  } = useTransportData(user);
+  } = useTransportData(appUser);
 
   // --- Auth Initialization ---
   useEffect(() => {
